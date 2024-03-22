eb17843566cde3c1841ad78b04378ea1d7604951
Jangchan0 <jchy16@gmail.com>
2024-03-22 15:41:01 +0900

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
     return (
         <>
-            <div>asdasd</div>
+            <div></div>
             <div />
         </>
     );
